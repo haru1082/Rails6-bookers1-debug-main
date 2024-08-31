20360f804606c71c1f43ae70b0c2ee608bc5be5c
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-08-31 14:33:12 +0000

    debug完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  def top
+  def new
+    @book = Book.new
   end
 
   def index
@@ -13,7 +14,10 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      flash.now[:alert] = "error:Posting failed"
+      @books = Book.all
       render :index
+      
     end
   end
 
@@ -27,10 +31,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
+      flash.now[:alert] = "error: Update failed"
       render :edit
     end
   end
